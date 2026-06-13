767d27f1ed12a136cd7701d6479c7447d79e25cb
bhavyamishra9119-blip <bhavyamishra9119@gmail.com>
2026-06-13 16:21:23 +0530

    initial commit

--- 1. Learn the Basics/5. Learn Basic Recursion/01_Printing_N_times.cpp
+++ 1. Learn the Basics/5. Learn Basic Recursion/01_Printing_N_times.cpp
@@ -1,7 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    
+void N_time(string x, int count, int K)
+{
+    if (count == K)
+    {
+        return;
+    }
+
+    cout << x << endl;
+    N_time(x, count + 1, K);
+}
+
+int main() 
+{
+    string x = "Horse";
+    int K = 5;
+
+    N_time(x, 0, K);
+
     return 0;
 }
