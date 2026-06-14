4468a9166e9476e704ad0ae1a8f44377944fab6f
Bhavya Mishra <bhavyadivya1972@outlook.com>
2026-06-14 13:14:18 +0530

    Initial commit

---
+++ 1. Learn the Basics/5. Learn Basic Recursion/05_Sum_of_first_N_numbers.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution 
+{
+    public:
+    int sum_N_nos(int n, int count, int sum)
+    {
+        if (count > n)
+        {
+            return sum;
+        }
+        return sum_N_nos(n, count + 1, sum += count);
+    }
+};
+
+int main() {
+    int n = 5;
+
+    Solution Sol;
+    cout << Sol.sum_N_nos(n, 1, 0);
+    return 0;
+}
